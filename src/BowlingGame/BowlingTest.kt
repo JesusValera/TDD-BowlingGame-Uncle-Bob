c680de996d860525f541039aeb2d52dbbb815855
Jesus Valera <jesus1902@outlook.com>
2018-10-07 23:58:44 +0200

    5. Spare method.

--- src/BowlingGame/BowlingTest.kt
+++ src/BowlingGame/BowlingTest.kt
@@ -1,5 +1,6 @@
 package BowlingGame
 
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
@@ -7,25 +8,35 @@ class BowlingTest {
 
     private val game: Game = Game()
 
-    @Test
-    fun canRoll() {
-        game.roll(0)
+    private fun rollMany(n: Int, pins: Int) {
+        for (i in 1..n) {
+            game.roll(pins)
+        }
+    }
+
+    private fun rollSpare() {
+        game.roll(5)
+        game.roll(5)
     }
 
     @Test
     fun gutterGame() {
-        for (i in 1..20) {
-            game.roll(0)
-        }
+        rollMany(20, 0)
         assertEquals(0, game.score())
     }
 
     @Test
     fun allOnes() {
-        for (i in 1..20) {
-            game.roll(1)
-        }
+        rollMany(20, 1)
         assertEquals(20, game.score())
+    }
+
+    @Test
+    fun oneSpare() {
+        rollSpare()
+        game.roll(3)
+        rollMany(17, 0)
+        assertEquals(16, game.score())
     }
 
 }
--- src/BowlingGame/Game.kt
+++ src/BowlingGame/Game.kt
@@ -2,12 +2,29 @@ package BowlingGame
 
 class Game {
 
-    private var score: Int = 0
+    private val rolls = IntArray(21)
+    private var currentRoll: Int = 0
 
     fun roll(pins: Int) {
-        this.score += pins
+        rolls[currentRoll++] = pins
     }
 
-    fun score(): Int = score
+    fun score(): Int {
+        var score = 0
+        var firstInFrame = 0
+
+        for (frame in 0 until 10) {
+            if (isSpare(firstInFrame)) {
+                score += 10 + rolls[firstInFrame + 2]
+            } else {
+                score += rolls[firstInFrame] + rolls[firstInFrame + 1]
+            }
+            firstInFrame += 2
+        }
+
+        return score
+    }
+
+    private fun isSpare(firstInFrame: Int) = rolls[firstInFrame] + rolls[firstInFrame + 1] == 10
 
 }
