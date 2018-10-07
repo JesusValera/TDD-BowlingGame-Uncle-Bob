fd403eb378a6e359d98e8437ff7b3fbf5414a525
Jesus Valera <jesus1902@outlook.com>
2018-10-07 22:47:59 +0200

    3. Gutter method.

--- src/BowlingGame/BowlingTest.kt
+++ src/BowlingGame/BowlingTest.kt
@@ -1,6 +1,7 @@
 package BowlingGame
 
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 class BowlingTest {
 
@@ -9,5 +10,13 @@ class BowlingTest {
     @Test
     fun canRoll() {
         game.roll(0)
+    }
+
+    @Test
+    fun gutterGame() {
+        for (i in 1..20) {
+            game.roll(0)
+        }
+        assertEquals(0, game.score())
     }
 }
